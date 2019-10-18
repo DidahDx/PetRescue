38e93d2d946bbca84dbba218f3ea1cd32daa6c4c
Daniel Didah <didahdaniel@gmail.com>
2019-10-18 11:20:59 +0300

    first commit

---
+++ app/src/main/java/com/example/petrescue/data/PetDbHelper.java
@@ -0,0 +1,54 @@
+package com.example.petrescue.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+import com.example.petrescue.data.PetContract.PetEntry;
+
+public class PetDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME="shelter.db";
+
+    /**
+     * Database version. If you change the database schema, you must increment the database version.
+     */
+    private static final int DATABASE_VERSION=1;
+
+    /**
+     * Constructs a new instance of {@link PetDbHelper}.
+     *
+     * @param context of the app
+     */
+    public PetDbHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     * This is called when the database is created for the first time.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //CREATE THE TABLE
+        String SQL_CREATE_PETS_TABLE="CREATE TABLE "+ PetEntry.TABLE_NAME + "("
+                + PetEntry._ID + "INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + PetEntry.COLUMN_PET_NAME + "TEXT NOT NULL,"
+                + PetEntry.COLUMN_PET_BREED + "TEXT ,"
+                + PetEntry.COLUMN_PET_GENDER + "INTEGER  NOT NULL ,"
+                + PetEntry.COLUMN_PET_WEIGHT + "INTEGER NOT NULL DEFAULT 0 );";
+
+        //Execute the SQL statement
+        db.execSQL(SQL_CREATE_PETS_TABLE);
+
+    }
+
+
+    /**
+     * This is called when the database needs to be upgraded.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // The database is still at version 1, so there's nothing to do be done here.
+    }
+}
